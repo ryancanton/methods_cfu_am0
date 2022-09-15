3797c17a1e0ebf1a0abfd58876bd578e341278d6
Ryan Canton <ryancanton999@gmail.com>
2022-09-15 12:26:20 -0600

    Upload completed versions of final_practice.rb and self_eval.md

--- final_practice.rb
+++ final_practice.rb
@@ -1,38 +1,68 @@
 # Defining Our Own Methods
 # 1: Write a method named greeting that prints out a general greeting to someone
+def greeting
+  p "Top of the mornin' to ya laddy! How's ya doin' today?"
+end
+
+greeting
 
 # What is the return value of your method?
+# "Top of the mornin' to ya laddy! How's ya doin' today?"
+
 # How many arguments did you pass your method?
+# 0
 
 
 
 
 #2: Write a method named custom_greeting that prints out a greeting WITH a specific name.
+def custom_greeting(name)
+  p "Top of the mornin' to ya laddy! How's #{name} doin' today?"
+end
 
+custom_greeting("Billay")
 
 # What is the return value of your method?
+# "Top of the mornin' to ya laddy! How's Billay doin' today?" (the String will change depending on the argument)
+
 # How many arguments did you pass your method?
+# 1
+
 # What data type was your argument(s)?
+# String
 
 
 
 
 #3: Write a method named square that takes in one number, and returns the square of that number
 
+def square(num)
+  num * num
+end
 
+p square(9)
 
 # What is the return value of your method?
+# 81
 # How many arguments did you pass your method?
+# 1 argument
 # What data type was your argument(s)?
+# Integer
 
 
 
 
 #4: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and print outs the sentence of the entire string
 
+def greet_person(first, middle, last)
+  p "Thine face is fair, #{first} #{middle} #{last}."
+end
 
+greet_person("Jorge", "Francis", "Nixon")
 
 # What is the return value of your method?
+# "Thine face is fair, Jorge Francis Nixon."
 # How many arguments did you pass your method?
+# 3
 # What data type was your argument(s)?
-
+# String
--- self_eval.md
+++ self_eval.md
@@ -1,5 +1,17 @@
 # Self Evaluation
 
 - How confident do you feel about your use of built-in Ruby methods?
+
+I feel completely confident
+
 - How confident do you feel writing simple methods of your own?
+
+I feel completely confident
+
 - What questions do you have about methods, and what they are capable of? (ask a question!!!)
+
+Is the only way to ensure no user error happens when inputting arguments through conditionals?
+For example, if I only string is the only acceptable datatype for a method parameter, would one have
+to put a conditional checking if the argument passed through is a string? Is there a way
+to set the parameter's datatype in the method definition? This becomes especially important
+for methods with multiple parameters that need to be different datatypes.
