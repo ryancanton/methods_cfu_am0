afdc35e41b8144a1fdd021b1280cac8e20b40398
Ryan Canton <ryancanton999@gmail.com>
2022-09-15 11:38:54 -0600

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -9,19 +9,27 @@
 
 # YOU DO the rest:
 
-
+# The .include? method is called on the string object "Hello World"
+# "Hello" is passed as an argument, and the method checks to see if the argument is included in the original string
+# The return value is true
 "Hello World".include?("Hello")
 
-
+# The .end_with? method is called on the string object "Hello World"
+# "Hello" is passed in as an argument, and the method checks if the argument matches the last five characters of the string
+# The return value is false, because "Hello" does not equal "World"
 "Hello World".end_with?("Hello")
 
-
+# The .end_with? method is called on the string object "Hello World"
+# "rld" is passed in as an argument, and the method checks if the argument matches the last three characters of the string
+# The return value is true, because "rld" does equal "rld"
 "Hello World".end_with?("rld")
 
-
+# The .even? method is called on the integer 12
+# There is no argument because the method has no parameters. The method checks if the value that it was called on is even
+# 12 is even, so the return value is true
 12.even?
 
-
+# The .next method is called on the integer 18
+# There is no argument because the method has no parameters. The method outputs the next integer after whatever it was called on.
+# The return value is 19
 18.next
-
-
